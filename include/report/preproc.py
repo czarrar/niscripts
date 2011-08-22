1f907dc84a88b3c7ba46b4978007b74bb191fe8d
Zarrar Shehzad <czarrar@gmail.com>
2011-08-22 17:33:39 -0400

    fix

--- include/report/preproc.py
+++ include/report/preproc.py
@@ -35,7 +35,7 @@ class PreprocReporter(Reporter):
             f.close()
         return
     
-    def _psub(path):
+    def _psub(self, path):
         return self.relpath(self._substitute(x), self.outdir)
     
     def addSubject(self, subject, anatdirs, regdirs, funcdirs):
