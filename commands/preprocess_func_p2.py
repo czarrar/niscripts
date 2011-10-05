e660e9ddfdc6b8310479fd89e8cb57fe6fd3532c
Zarrar Shehzad <czarrar@gmail.com>
2011-10-05 15:19:15 -0400

    test

--- commands/preprocess_func_p2.py
+++ commands/preprocess_func_p2.py
@@ -153,8 +153,8 @@ def functional_preprocessing(
     datasource.inputs.base_directory=os.path.abspath(inputs.basedir)
     datasource.inputs.template = "*"
     datasource.inputs.field_template = dict(
-        func = os.path.join("%s", inputs.funcdir, "run_[0-9]*", inputs.infunc),
-        func_mask = os.path.join("%s", inputs.funcdir, "run_[0-9]*", inputs.inmask)
+        func = os.path.join("%s", inputs.funcdir, inputs.infunc),
+        func_mask = os.path.join("%s", inputs.funcdir, inputs.inmask)
     )
     datasource.inputs.template_args = dict(
                                         func = [['subject_id']],
