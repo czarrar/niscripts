56118e05643047080c5e65a9f47e19e498d107da
Zarrar Shehzad <czarrar@gmail.com>
2011-10-05 15:23:09 -0400

    test

--- commands/preprocess_func_p2.py
+++ commands/preprocess_func_p2.py
@@ -164,9 +164,6 @@ def functional_preprocessing(
     # Get the number of runs for each participant (for renaming purposes)
     datasource.inputs.subject_id = subject_list
     ds = datasource.run()
-    print [ x for x in ds.outputs.func ]
-    print ds.outputs.func
-    raise SystemExit(1)
     runs = [ len(x) for x in ds.outputs.func ]  # list with number of runs per subject
     max_runs = max(runs)
     
